2285f063e7a2ca01a8d7217072d2a264d97d188c
Sandip <roysandip33290@gmail.com>
2024-10-15 10:45:49 +0530

    fixed some bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "Institute Management Dashboard",
+  description: "Next.js Institute Management System",
 };
 
 export default function RootLayout({
