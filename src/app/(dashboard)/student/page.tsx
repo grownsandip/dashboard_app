1b7e3343df823183dff022623131b53ea886f8b5
Sandip <roysandip33290@gmail.com>
2024-11-17 12:59:36 +0530

    student's page database integration done

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,16 +1,25 @@
 import Announcements from '@/components/Announcements'
-import BigCalendar from '@/components/BigCalendar'
+import BigCalenderContainer from '@/components/BigCalenderContainer';
 import EventCalender from '@/components/EventCalender'
+import prisma from '@/lib/prisma'
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
-const StudentPage = () => {
+const StudentPage = async () => {
+  const {userId}=await auth()
+  const classItem=await prisma.class.findMany({
+    where:{
+      students:{some:{id:userId!}}
+    }
+  })
+  //console.log(classItem)
   return (
     <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/* left */}
       <div className='w-full xl:w-2/3'>
         <div className='bg-white rounded-md p-4 h-full'>
-          <h1 className='text-xl font-semibold'>Schedule(4A)</h1>
-          <BigCalendar />
+          <h1 className='text-xl font-semibold'>Schedule of {classItem[0].semId}th semester</h1>
+          <BigCalenderContainer type="classId" id={classItem[0].id}/>
         </div>
       </div>
       {/* right */}
